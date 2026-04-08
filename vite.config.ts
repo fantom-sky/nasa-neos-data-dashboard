c554402b08e48800065a39d60ede4fb1a88e57e2
OLga I. Mikhalchenko <olga.i.mikhalchenko@gmail.com>
2026-04-08 17:04:16 +0300

    Adds deployment scripts and vite config for production

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/nasa-neos-data-dashboard/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
